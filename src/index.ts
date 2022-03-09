4b38c0431cde8ef85f35b61bfc62b0fd383c631c
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2022-03-09 17:54:27 +0300

    Edit: general export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
+/**
+ * Components
+ */
+export * from "./components/Alert";
 export * from "./components/Button";
 export * from "./components/CheckBox";
 export * from "./components/Icon";
 export * from "./components/Loader";
 export * from "./components/Space";
 export * from "./components/Text";
+/**
+ * Hooks
+ */
+export * from "./hooks/useAlert";
+/**
+ * Export variables
+ */
 export * as design from "./styles/_export.module.scss";
